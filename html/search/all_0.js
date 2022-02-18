b7bdfd54f89ce85d617d67363397512b8a59c3b3
Laxe <houlipatch@gmail.com>
2022-02-18 20:54:58 +0100

    Documentation for the NeuralNet branch.

---
+++ html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['datasetprocessor_0',['DatasetProcessor',['../class_dataset_processor_1_1_dataset_processor.html',1,'DatasetProcessor']]]
+];
---
+++ html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['networkcontainer_1',['NetworkContainer',['../classmain_1_1_network_container.html',1,'main']]]
+];
