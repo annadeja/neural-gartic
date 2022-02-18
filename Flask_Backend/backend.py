976cd5a5ec5cffb641fd9aa3017cf32bbcd377d5
Mateusz Chłopek <matikc25@gmail.com>
2022-02-18 21:56:53 +0100

    sadge

--- Flask_Backend/backend.py
+++ Flask_Backend/backend.py
@@ -1,98 +1,27 @@
-import uuid
 from flask import Flask
-from flask_session import Session
 from flask import render_template, request, redirect, url_for, session
-import os
-import random
 
 
 app = Flask(__name__)
-sess = Session()
-
-ROUNDS_COUNT = 5
-
-games = []
-# one_game =
-# {
-#   id: int,
-#   info: {
-#       round: int,
-#       topics: [],
-#       images: [],
-#       predictions: []
-#       }
-# }
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 
-@app.route('/game', methods=['GET', 'POST'])
+@app.route('/game', methods=['POST'])
 def game():
-    if request.method == 'GET':
-        game_id = create_new_game()
-        session['game'] = game_id
+    img = request.files['file'].read()
 
-        for game in games:
-            if game['id'] == game_id:
-                round = game['info']['round']
-                return render_template('game.html', topic=game['info']['topics'][round])
-
-    elif request.method == 'POST':
-        # get the image but check if there is any image!!!
-        # image = request.files['img']
-        game_id = session['game']
-        for game in games:
-            if game['id'] == game_id:
-                game['info']['round'] += 1
-                round = game['info']['round']
-
-                if round == ROUNDS_COUNT:
-                    return redirect(url_for('end'))
-
-                #game['info']['images'].append(image)
-
-                return render_template('game.html', topic=game['info']['topics'][round])
-    else:
-        return 'Coś poszło nie tak...'
+    return prediction(None)
 
 
-@app.route('/end')
-def end():
-    return render_template('end.html')
-
-
-def get_random_topic() -> str:
-    all_topic = ['one', 'two', 'three']
-    return random.choice(all_topic)
-
-
-def create_new_game() -> int:
-    unique_game_id = uuid.uuid4().int
-
-    topics = []
-    for i in range(ROUNDS_COUNT):
-        topics.append(get_random_topic())
-
-    games.append({
-        'id': unique_game_id,
-        'info': {
-            'round': 0,
-            'topics': topics,
-            'images': [],
-            'predictions': []
-        }
-    })
-
-    return unique_game_id
+def prediction(image) -> str:
+    return 'wolę_równania_stanu_niż_to'
 
 
 if __name__ == "__main__":
-    app.secret_key = 'citrag'
-    app.config['SESSION_TYPE'] = 'filesystem'
-    sess.init_app(app)
     app.debug = True
     app.run()
 
